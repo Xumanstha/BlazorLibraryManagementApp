85b64e8ce3e487a8750ea1a4f06a7b6d94e91a66
Xumanstha <shresthaangura@gmail.com>
2025-01-14 09:08:39 +0545

    complete student list corrected

---
+++ StudentList.cs
@@ -0,0 +1,8 @@
+public class StudentList{
+       public List<student> students = new List<student>()
+{
+new student("1", "Suman", "Male", "Devinagar", "0299028982", "BScCSIT", "3"),
+new student("2", "Saroj", "Male", "Drivertole", "786382638", "BScCSIT", "3"),
+new student("3", "Bikram", "Male", "Devinagar", "89374988747", "BScCSIT", "3")
+};
+}
