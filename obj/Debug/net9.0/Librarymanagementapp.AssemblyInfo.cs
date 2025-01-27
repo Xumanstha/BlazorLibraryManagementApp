dac2b8e3f4ddfc3e01aba730d38d0b85a26e8c37
Xumanstha <shresthaangura@gmail.com>
2025-01-27 12:44:21 +0545

    student menu

--- obj/Debug/net9.0/Librarymanagementapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/Librarymanagementapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Librarymanagementapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+320386510b0a58bb50aa850c2fdc42c2efe3f95b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e0356a16fa281a843efa7ed352f50689ca07581")]
 [assembly: System.Reflection.AssemblyProductAttribute("Librarymanagementapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Librarymanagementapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
